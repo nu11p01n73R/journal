f478a8e999689a0aa72520c7cc5041ee96960516
nu11p01n73R <kjjohny1991@gmail.com>
2018-05-14 21:59:07 +0530

    Add journal script

---
+++ journal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+input="$@"
+file_name=$(gdate -d "$input" "+${JOURNAL_PATH:-$HOME/.journal}/%Y/%B/%d.md") && \
+        mkdir -p $(dirname $file_name) && \
+        ${EDITOR:-vim} $file_name
